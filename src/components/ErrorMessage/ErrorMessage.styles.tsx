eebb1cb3f06a20469a1cb3f7e59991c75ec266da
Lucio Merotta <lucio@swibeco.ch>
2021-03-30 14:38:39 +0200

    use real oauth ! remove motion.custom() calls

--- src/components/ErrorMessage/ErrorMessage.styles.tsx
+++ src/components/ErrorMessage/ErrorMessage.styles.tsx
@@ -1,4 +1,4 @@
 import tw from "twin.macro";
 import { motion } from "framer-motion";
 
-export const Wrapper = motion.custom(tw.span`text-red-600 text-sm`);
+export const Wrapper = motion(tw.span`text-red-600 text-sm`);
--- src/components/Input/Input.styles.tsx
+++ src/components/Input/Input.styles.tsx
@@ -1,7 +1,7 @@
 import tw, { styled } from "twin.macro";
 import { motion } from "framer-motion";
 
-export const Wrapper = motion.custom(tw.div`relative outline-none`);
+export const Wrapper = motion(tw.div`relative outline-none`);
 
 export const labelFilledStyles = tw`text-primary top-0 translate-y-0.5 text-xs`;
 
@@ -9,7 +9,7 @@ export const Label = styled.label`
   ${tw`absolute top-1/2 transform -translate-y-1/2 left-3 text-default cursor-text transition-all pointer-events-none`}
 `;
 
-export const InputWrapper = motion.custom(tw.div`w-full relative`);
+export const InputWrapper = motion(tw.div`w-full relative`);
 export const Input = styled.input<{ invalid?: boolean }>`
   ${tw`w-full pt-4 pb-2 pl-3 pr-3 border rounded border-gray-400 appearance-none outline-none transition-colors focus:border-primary`}
   &:focus {
